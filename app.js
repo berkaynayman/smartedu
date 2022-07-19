3fbe6d3c6bd3c8f0718fb3c792b51125ceddbd79
berkaynayman <berkaynayman4@gmail.com>
2022-07-19 22:28:54 +0300

    Routing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const pageRoute = require('./routes/pageRoute') 
 
 const app = express()
 
@@ -8,20 +9,8 @@ app.set("view engine", "ejs")
 // Middlewares
 app.use(express.static("public"))
 
-
 // Routes
-app.get('/', (req, res) => {
-    res.status(200).render('index', {
-        page_name: "index"
-    })
-})
-
-
-app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-        page_name: "about"
-    })
-})
+app.use('/', pageRoute)
 
 const port = 3000
 app.listen(port, () => {
