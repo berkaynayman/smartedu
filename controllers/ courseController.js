2fe3d8c67f578475694ba18b17e0d7c302d111ee
berkaynayman <berkaynayman4@gmail.com>
2022-07-27 19:12:57 +0300

    Course page. Slugify.

--- controllers/ courseController.js
+++ controllers/ courseController.js
@@ -1,7 +1,6 @@
 const Course = require('../model/Course')
 
 exports.createCourse = async (req, res) => {
-
     try {
         const course = await Course.create(req.body)
 
@@ -18,13 +17,28 @@ exports.createCourse = async (req, res) => {
 }
 
 exports.getAllCourses = async (req, res) => {
-
     try {
         const courses = await Course.find()
 
         res.status(200).render('courses', {
             page_name: "courses",
             courses
+        })
+    } catch(error) {
+        res.status(400).json({
+            status: 'fail',
+            error
+        })
+    }
+}
+
+exports.getCourse = async (req, res) => {
+    try {
+        const course = await Course.findOne({slug: req.params.slug})
+
+        res.status(200).render('course', {
+            page_name: "courses",
+            course
         })
     } catch(error) {
         res.status(400).json({
