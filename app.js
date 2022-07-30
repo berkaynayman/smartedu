bf4eb6c1e262286a1f5d0b2a9bc16b6895a4a392
berkaynayman <berkaynayman4@gmail.com>
2022-07-30 17:51:08 +0300

    Register page, Bcrypt package. Login page, login function. Session. Logout.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const pageRoute = require('./routes/pageRoute') 
 const courseRoute = require('./routes/courseRoute')
 const categoryRoute = require('./routes/categoryRoute')
+const userRoute = require('./routes/userRoute')
+
 
 const app = express()
 
@@ -17,16 +21,29 @@ mongoose.connect('mongodb://127.0.0.1:27017/smartedu-db', {
 // Template engine
 app.set("view engine", "ejs")
 
+// Global Variable
+global.userIN = null
+
 // Middlewares
 app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.use(session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/smartedu-db'})
+}))
 
 // Routes
+app.use('*', (req, res, next) => {
+    userIN = req.session.userID
+    next()
+})
 app.use('/', pageRoute)
 app.use('/courses', courseRoute)
 app.use('/categories', categoryRoute)
+app.use('/users', userRoute)
 
 const port = 3000
 app.listen(port, () => {
