9407279f9145e34311b27c012fced49c581afb24
berkaynayman <berkaynayman4@gmail.com>
2022-08-07 08:50:30 +0300

    Course and teacher connection.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,10 +3,15 @@ const Category = require('../model/Category')
 
 exports.createCourse = async (req, res) => {
     try {
-        const course = await Course.create(req.body)
+        const course = await Course.create({
+            name: req.body.name,
+            description: req.body.name,
+            category: req.body.category,
+            user: req.session.userID
+        })
 
         res.status(201).redirect('/courses')
-    } catch(error) {
+    } catch (error) {
         res.status(400).json({
             status: 'fail',
             error
@@ -43,7 +48,7 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
     try {
-        const course = await Course.findOne({slug: req.params.slug})
+        const course = await Course.findOne({slug: req.params.slug}).populate('user')
 
         res.status(200).render('course', {
             page_name: "courses",
