2dadab700bc72edff8c28265764edc51aea2a39f
berkaynayman <berkaynayman4@gmail.com>
2022-09-05 20:09:44 +0300

    Course and student connection.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,11 +1,12 @@
 const Course = require('../model/Course')
 const Category = require('../model/Category')
+const User = require('../model/User')
 
 exports.createCourse = async (req, res) => {
     try {
         const course = await Course.create({
             name: req.body.name,
-            description: req.body.name,
+            description: req.body.description,
             category: req.body.category,
             user: req.session.userID
         })
@@ -54,6 +55,21 @@ exports.getCourse = async (req, res) => {
             page_name: "courses",
             course
         })
+    } catch(error) {
+        res.status(400).json({
+            status: 'fail',
+            error
+        })
+    }
+}
+
+exports.enrollCourse = async (req, res) => {
+    try {
+        const user = await User.findById(req.session.userID)
+        await user.courses.push({_id:req.body.course_id})
+        await user.save()
+
+        res.status(200).redirect('/users/dashboard')
     } catch(error) {
         res.status(400).json({
             status: 'fail',
