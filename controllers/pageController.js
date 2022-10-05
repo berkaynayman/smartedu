54ca2266480386f91c20db847d0e2b2fef131903
berkaynayman <berkaynayman4@gmail.com>
2022-10-05 13:28:53 +0300

    Notification sending.

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -32,43 +32,49 @@ exports.getContactPage = (req, res) => {
 
 exports.sendEmail = async (req, res) => {
     
-    const outputMessage = `    
-    <h1>Mail Details</h1>
-    <ul>
-        <li>Name: ${req.body.name}</li>
-        <li>Email: ${req.body.email}</li>
-    </ul>
-    <h1>Message</h1>
-    <p>${req.body.message}</p>
-    `
+    try {
+        const outputMessage = `    
+            <h1>Mail Details</h1>
+            <ul>
+                <li>Name: ${req.body.name}</li>
+                <li>Email: ${req.body.email}</li>
+            </ul>
+            <h1>Message</h1>
+            <p>${req.body.message}</p>
+        `
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-        user: "************@gmail.com", // gmail account
-        pass: "*************", // gmail pass for this smart edu app
-        },
-    });
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+            user: "************@gmail.com", // gmail account
+            pass: "*************", // gmail pass for this smart edu app
+            },
+        });
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"Smart edu contact form 👻" <***************>', // sender address
-        to: "*****************", // list of receivers
-        subject: "SMART EDU CONTACT FORM NEW MESSAGE ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: outputMessage, // html body
-    });
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Smart edu contact form 👻" <***************>', // sender address
+            to: "*****************", // list of receivers
+            subject: "SMART EDU CONTACT FORM NEW MESSAGE ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: outputMessage, // html body
+        });
 
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...  
 
-    res.status(200).redirect('contact')
+        req.flash("success", "We Received your message succesfully")
+    } catch (error) {
+        req.flash("error", `Something happened! ${error}`)
+    } finally {
+        res.status(200).redirect('contact')
+    }
 
 }
