bcf1bf9b337628ac9b7b8814e66358cfba6ce1aa
berkaynayman <berkaynayman4@gmail.com>
2022-10-07 23:18:51 +0300

    Register validation.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt')
+const { validationResult } = require('express-validator');
 const User = require('../model/User')
 const Category = require('../model/Category')
 const Course = require('../model/Course')
@@ -9,10 +10,12 @@ exports.createUser = async (req, res) => {
 
         res.status(201).redirect('/login')
     } catch (error) {
-        res.status(400).json({
-            status: 'fail',
-            error
-        })
+        const errors = validationResult(req)
+
+        for(let i=0; i < errors.array().length; i++){
+            req.flash("error", errors.array()[i].msg)
+        }
+        res.status(400).redirect('/register')
     }
 }
 
@@ -24,10 +27,18 @@ exports.loginUser = async (req, res) => {
 
         if(user) {
             bcrypt.compare(password, user.password, (err, same) => {
-                // USER SESSION
+                if(same) {
+                    // USER SESSION
                     req.session.userID = user._id
                     res.status(200).redirect('/users/dashboard')
+                } else {
+                    req.flash("error", "Your password is not correct!")
+                    res.status(400).redirect('/login')
+                }
             })
+        } else {
+            req.flash("error", "User is not exist!")
+            res.status(400).redirect('/login')
         }
     } catch (error) {
         res.status(400).json({
