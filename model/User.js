d97fa307b1a5f8e363b67b8c0514d6c0d93d8977
berkaynayman <berkaynayman4@gmail.com>
2022-10-08 06:57:58 +0300

    Course delete. Some login problems solved.

--- model/User.js
+++ model/User.js
@@ -30,7 +30,10 @@ const UserSchema = new Schema({
 
 UserSchema.pre('save', function(next) {
     const user = this
-    bcrypt.hash(user.password, 10, (error, hash) => {
+    if (!user.isModified('password')) return next()
+
+    bcrypt.genSalt(user.password, 10, (error, hash) => {
+        if (error) return next(error)
         user.password = hash
         next()
     })
